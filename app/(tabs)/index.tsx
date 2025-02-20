7ea3d4abc74bc9af3eff17ff62fc306cdef539e3
Macarenapineiro1 <macarena.pineiro@correo.ucu.edu.uy>
2025-02-20 11:31:36 -0300

    Cree el calendario

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,28 +6,17 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { HomePage } from '@/components/HomePage';
 import { BotonesMainPage } from '@/components/BotonesMainPage';
+import Calendar from '@/components/calendar';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <BotonesMainPage/>
-    </ParallaxScrollView>
+    <HomePage/>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
+    flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
     gap: 8,
--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet, View, Text, TouchableHighlight } from "react-native";
+import Calendar from '@/components/calendar';
 
 export function HomePage() {
     return (
@@ -6,14 +7,14 @@ export function HomePage() {
             <TouchableHighlight
             style={{
                 width: 100,
-                height: 100,
+                height: 50,
                 backgroundColor: "black",
                 borderRadius: 5,
                 justifyContent: "center",
                 alignItems: "center",
             }}
             >
-            <Text style={{ color: "black" }}>Iniciar sesión</Text>
+            <Text style={{ color: "white" }}>Iniciar sesión</Text>
             </TouchableHighlight>
       </View>
     );
@@ -23,11 +24,11 @@ export function HomePage() {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: "black",
+      backgroundColor: "white",
       alignItems: "center",
       justifyContent: "center",
-      width: 100,
-      height: 100,
+      width: "100%",
+      height: "100%",
 
     },
   });
