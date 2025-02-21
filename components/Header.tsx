a095cfd9c8d94a91a60c34ea2abba4348c020406
Ignacio Correa <ignaciocorrea@MacBook-Pro-de-Ignacio.local>
2025-02-21 07:51:23 -0300

    Header para revisar

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,10 +7,10 @@ const ituzaingoImage = require("../assets/images/ituzaingo.png");
 const CustomHeader = ({ title }: { title: string }) => {
   return (
     <View style={styles.header}>
-      {/* Imagen con margen a la izquierda */}
+      
       <Image style={styles.logo} source={ituzaingoImage} />
       
-      {/* Texto del header con color negro */}
+      
       <Text style={styles.headerText}>{title}</Text>
     </View>
   );
@@ -20,22 +20,22 @@ const styles = StyleSheet.create({
   header: {
     width: "100%",
     height: 60,
-    backgroundColor: "transparent", // Cambio: Se eliminó el color de fondo
-    flexDirection: "row", // Alinea imagen y texto en fila
+    backgroundColor: "transparent",
+    flexDirection: "row", 
     alignItems: "center",
-    justifyContent: "center", // Cambio: Alineación a la izquierda
+    justifyContent: "center",
     paddingHorizontal: 15,
     marginTop: 35,
   },
   logo: {
     width: 40,
     height: 40,
-    marginRight: 25, // Espacio entre la imagen y el texto
-    marginLeft: -5, // Cambio: Mueve la imagen un poco a la izquierda
+    marginRight: 25, 
+    marginLeft: -5, 
   },
   headerText: {
     fontSize: 25,
-    color: "#000", // Cambio: Texto en color negro
+    color: "#000", 
     fontWeight: "bold",
   },
 });
