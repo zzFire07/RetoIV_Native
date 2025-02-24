19d2fe22ac5765c0caf4625bb1f2bf21fe8c4023
Macarenapineiro1 <macarena.pineiro@correo.ucu.edu.uy>
2025-02-24 12:14:55 -0300

    implemente Axios

--- app/TicketPage.tsx
+++ app/TicketPage.tsx
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, StyleSheet, Button, TouchableHighlight, useColorScheme } from "react-native";
 import TicketComponent from "@/components/TicketComponent";
 import CustomHeader from "@/components/CustomHeader";
+import axios from "axios";
+
 
 export function TicketPage(){
     const colorScheme = useColorScheme();
-    const listatickets = [
+    const lista = [
         { id: 1, name: "Ticket 1" },
         { id: 2, name: "Ticket 2" },
         { id: 3, name: "Ticket 3" },
     ];
+
+    const [listaTicket, setListaTicket] = useState(lista);
+
+    useEffect(()=>{
+        // Se intenta traer informacion desde la base de datos, si no se puede se setea la informacion provisoria.
+        axios.get('http://localhost:3000/traerpaquetes',{
+          headers: {
+            'Access-Control-Allow-Origin' : '*'
+          },
+          responseType: "json",
+        }).then((response) => {
+        // Si el back-end esta corriendo correctamente, se setea la informacion de la base de datos.
+          setListaTicket(response.data);
+          console.log('restaurantes', response.data)
+        }).catch((error) => {
+        // Si el back-end no esta corriendo, se setea la informacion provisoria.
+        })
+    
+    },[])
+
     return (
         <>
         <CustomHeader title="Club Ituzaingo" />
         <View style={styles.container}>
             <Text style={styles.title}>Ticketeras Disponibles</Text>
-            {listatickets.map((ticket)=>(
+            {listaTicket.map((ticket)=>(
                 <TicketComponent
                 key={ticket.id}
                 id={ticket.id}
