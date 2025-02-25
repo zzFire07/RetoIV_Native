3b5ee9dbbc22f97638bb6d6b8629cf333d2bd1ba
candelamangino <candelamangino@gmail.com>
2025-02-25 09:38:40 -0300

    boton que redirecciona a whatsapp

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,15 +1,28 @@
 import AuthenticationButton from "./AuthenticationButton";
 import React, { useState } from "react";
 import CustomHeader from "./CustomHeader";
-import { Text, TouchableOpacity, StyleSheet } from "react-native";
+import { Text, TouchableOpacity, StyleSheet, Linking  } from "react-native";
+
 export function HomePage() {
   const [loggedIn, setLoggedIn] = useState(false);
+const handleWhatsAppPress=() => {
+  const phoneNumber="59894431672";
+  const whatsappURL=`https://wa.me/${phoneNumber}`;
+  Linking.openURL(whatsappURL).catch(() =>
+    alert("no se pudo abrir WhatsApp")
+  );
+};
   
     return (
       <>
         <CustomHeader title="Club Ituzaingo" />
         <TouchableOpacity style={styles.button} onPress={() => setLoggedIn(!loggedIn)}>
           <Text>{loggedIn ? "Cerrar Sesión" : "Iniciar Sesión"}</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.whatsappButton}
+        onPress={handleWhatsAppPress}>
+          <Text style={styles.whatsappText}> Contactar con Administrador</Text>
         </TouchableOpacity>
         {!loggedIn && <AuthenticationButton />}
         {loggedIn && (
@@ -29,5 +42,18 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     borderRadius: 8,
     alignItems: "center",
+  },
+  whatsappButton: {
+    backgroundColor:"#25D366",
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  whatsappText:{
+    color:"white",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
