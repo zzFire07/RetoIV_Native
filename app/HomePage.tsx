3d17e8193c064a4d49b5a6f9de6d785c1f028e0b
candelamangino <candelamangino@gmail.com>
2025-02-26 08:50:37 -0300

    agregue el boton de wpp y en vez de texto puse la imagen del logo

--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -5,6 +5,7 @@ import { Text, TouchableOpacity, StyleSheet, View} from "react-native";
 import BuyTicketsButton from "../components/BuyTicketsButton";
 import MatchDisponibility from "../components/MatchDisponibility";
 import { useAppContext } from "@/context/AppContext";
+import WhatsAppButton from "@/components/unused-comps/WhatsAppButton";
 
 export function HomePage() {
   const { loggedIn, setLoggedIn } = useAppContext();
@@ -15,6 +16,7 @@ export function HomePage() {
         <TouchableOpacity style={styles.button} onPress={() => setLoggedIn(!loggedIn)}>
           <Text>{loggedIn ? "Cerrar Sesión" : "Iniciar Sesión"}</Text>
         </TouchableOpacity>
+        
 
         <View style={styles.container}>
           {!loggedIn && <AuthenticationButton />}
@@ -24,8 +26,11 @@ export function HomePage() {
               <BuyTicketsButton />
             </>
           )}
+
+          
         
         </View>
+        <WhatsAppButton />
       </>
     );  
 }
--- components/unused-comps/WhatsAppButton.tsx
+++ components/unused-comps/WhatsAppButton.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, TouchableOpacity, StyleSheet, Linking } from "react-native";
+import {TouchableOpacity, Image,  StyleSheet, Linking } from "react-native";
 
 const handleWhatsAppPress = () => {
   const phoneNumber = "59899532260";
@@ -13,23 +13,26 @@ const handleWhatsAppPress = () => {
 export default function WhatsAppButton() {
   return (
     <TouchableOpacity style={styles.whatsappButton} onPress={handleWhatsAppPress}>
-      <Text style={styles.whatsappText}>Contactar con Administrador</Text>
+      <Image source={require("../../assets/images/whatsapp-logo.jpg")}
+      style={styles.whatsappIcon} 
+      />
     </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   whatsappButton: {
-    backgroundColor: "#25D366",
-    paddingVertical: 12,
-    paddingHorizontal: 20,
-    borderRadius: 8,
-    alignItems: "center",
-    marginTop: 20,
+    position: "absolute",
+    bottom: 60,
+    right: 20,
+    zIndex: 10,
+    marginTop: 50,
+    marginBottom: 50,
+  
   },
-  whatsappText: {
-    color: "white",
-    fontSize: 16,
-    fontWeight: "bold",
+  whatsappIcon: {
+    width: 50,
+    height: 50,
+    resizeMode: "contain",
   },
 });
